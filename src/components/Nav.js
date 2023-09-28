3459d74a5d2b78f8cae23fda7d23c9801fe985dc
Kelley Skinner <skinner677@gmail.com>
2023-09-28 10:56:45 -0400

    delete app.css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,12 @@ import React from "react";
 
 const Nav = () => (
         <nav>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
-            </ul>
+            <a href="/home">Home</a>
+            <a href="/about">About</a>
+            <a href="/menu">Menu</a>
+            <a href="reservations">Reservations</a>
+            <a href="/order-online">Order Online</a>
+            <a href="/login">Login</a>
         </nav>
 );
 
