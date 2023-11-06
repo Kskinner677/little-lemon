8e23077f686ef1d288ec9f9bb25204e7db2883d8
Kelley Skinner <skinner677@gmail.com>
2023-11-06 11:06:45 -0500

    update hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,19 +1,42 @@
 import React from "react";
 import FullScreenSection from "./FullScreenSection";
-import { Image, Heading, HStack, VStack, Box, Text } from "@chakra-ui/react";
+import { Image, Heading, HStack, VStack, Box, Text, Button, ButtonGroup } from "@chakra-ui/react";
+import backgroundImage1 from "../assets/Mario-and-Adrian-b.jpg"
 
+const restuaruantName = "Little Lemon";
+const restuaruantLocation = "Chicago";
+const sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 const HeroSection = () => (
-<FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    backgroundColor="#Abc4cb"
-  >
-    <VStack>
-        <Heading as='h1' size="xl">Hero Section</Heading>
-        <Text>This is a test</Text>
+
+    <VStack
+      maxWidth="auto"
+      minHeight="100vh"
+      justifyContent="center"
+      alignItems="center"
+      bg="brand.primary.green"
+      color="brand.highlight.light"
+      >
+        <HStack
+          spacing={8}
+          padding={16}
+        >
+          <Box>
+            <Heading as='h1' size="4xl">{restuaruantName}</Heading>
+            <Heading as='h1' size="2xl">{restuaruantLocation}</Heading>
+            <Text>{sampleText}</Text>
+          </Box>
+          <Box>
+            <Image src={backgroundImage1}></Image>
+          </Box>
+        </HStack>
+        <ButtonGroup alignItems="center" justifyContent="center" gap='4'>
+          <Button colorScheme='whiteAlpha'>ORDER ONLINE</Button>
+          <Text>or</Text>
+          <Button colorScheme='blackAlpha'>RESERVE A TABLE</Button>
+        </ButtonGroup>
     </VStack>
-</FullScreenSection>
+
 );
 
 export default HeroSection;
